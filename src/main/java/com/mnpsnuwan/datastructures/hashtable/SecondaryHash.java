b109f6697194a81ea0dee0d3faa386163a6de897
mnpsnuwan <nuwanp124@gmail.com>
2023-12-01 15:17:46 -0700

    IM:DEV:Datastructures - Hash Table Double Hashing Implementation and Unit Testing.

---
+++ src/main/java/com/mnpsnuwan/datastructures/hashtable/SecondaryHash.java
@@ -0,0 +1,16 @@
+package com.mnpsnuwan.datastructures.hashtable;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : datastructures
+ * Class : {@link SecondaryHash}
+ * User : Nuwan Samarasinghe | mnpsnuwan@gmail.com
+ * Date : 01Dec2023
+ * Time : 3:09 PM
+ */
+public interface SecondaryHash
+{
+
+    // A method for a second hash function
+    int hashCode2();
+}
